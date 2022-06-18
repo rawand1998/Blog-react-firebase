7239c5fc5bc1e89d9c58bd99ce6a692030e1c638
rawand1998 <rawandgaradh@gmail.com>
2022-06-18 19:11:44 +0300

    add post to firebase

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,34 +1,33 @@
-import React from 'react';
-import firebase from '../firebase'
-import {auth } from '../firebase'
-import {signInWithEmailAndPassword,createUserWithEmailAndPassword,} from'firebase/auth';
-import { addDoc,collection ,query} from '@firebase/firestore';
-import {db} from '../firebase'
+import React from "react";
+import firebase from "../firebase";
+import { auth } from "../firebase";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { addDoc, collection, query } from "@firebase/firestore";
+import { db } from "../firebase";
 // const postsCollectionRef = collection(db, "name");
 
 export const logInWithEmailAndPassword = async (email, password) => {
-    try {
-      await signInWithEmailAndPassword(auth,email, password);
- 
-    } catch (err) {
-      console.error(err);
-      alert(err.message);
-    }
-  };
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
 
-export const register= async(userName,email, password)=>{
-
-    try{
-        
-    const res=  await createUserWithEmailAndPassword(auth,email,password)
+export const register = async (userName, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
     //     const user= res.user
     //     console.log(addDoc(postsCollectionRef,{name:userName}),"cmjbned")
     //    addDoc(postsCollectionRef,{
     //     name:userName
     //    })
-    
-  
-      
-    }catch(err)
-    {console.error(err)}
-}
+  } catch (err) {
+    console.error(err);
+  }
+};
+
