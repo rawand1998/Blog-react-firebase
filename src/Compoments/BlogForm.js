2ad73c485ea01498c4a7a46d331eebb4a2057087
rawand1998 <rawandgaradh@gmail.com>
2022-06-19 20:51:32 +0300

    register and blog form style

--- src/Compoments/BlogForm.js
+++ src/Compoments/BlogForm.js
@@ -8,33 +8,37 @@ function BlogForm() {
   const [text, setText] = useState("");
   let navigate = useNavigate();
   const addPost = async (e) => {
-  
     e.preventDefault();
     const postRef = collection(db, "posts");
     const post = { title, text };
-   await addDoc(postRef, post).then(()=>{
-    navigate("/")
-   });
-  
+    await addDoc(postRef, post).then(() => {
+      navigate("/");
+    });
   };
 
   return (
-    <div>
-      <form>
-        <input
-          type="text"
-          placeholder="Add New Post"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Add New Post"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <button onClick={addPost}>Add Post</button>
-      </form>
+    <div className="login-container">
+      <div className="login">
+        <h3 className="login-word-post">Create New Post</h3>
+        <p className="login-welocme">Welcome onboard with us!</p>
+        <form className="login-form">
+          <label className="login-label">Title</label>
+          <input
+            type="text"
+            placeholder="Add New Post"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <label className="login-label">Text</label>
+          <input
+            type="text"
+            placeholder="Add New Post"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button onClick={addPost}>Add Post</button>
+        </form>
+      </div>
     </div>
   );
 }
